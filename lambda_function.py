e0a892efbb077815497c6a1aac73635f8be50241
mitsuhiro-yamashita <yamamanx@gmail.com>
2017-08-16 20:21:59 +0900

    logger debug可能にした

--- lambda_function.py
+++ lambda_function.py
@@ -9,33 +9,43 @@ import traceback
 
 
 slack_url = os.environ['SLACK_URL']
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
 
+logger = logging.getLogger()
 
-def sendMessage(content, channel):
+if log_level == 'ERROR':
+    logger.setLevel(logging.ERROR)
+elif log_level == 'DEBUG':
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
+def send_message(content, channel):
     payload_dic = {
         "text": content,
         "channel": channel,
     }
-    requests.post(slack_url, data=json.dumps(payload_dic))
+    logger.debug(payload_dic)
+    response = requests.post(slack_url, data=json.dumps(payload_dic))
+    logger.debug(response.text)
 
 
 def lambda_handler(event, context):
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
 
     try:
+        logger.debug(event)
         icon = event.get('icon',':question:')
+        logger.debug(icon)
 
-        sendMessage(
+        send_message(
             u':hourglass_flowing_sand:{icon}:申込がありました'.format(
                 icon=icon
             ),
             '#general'
         )
-
         return event
 
     except Exception as e:
-        sendMessage(traceback.format_exc(), '#general')
+        send_message(traceback.format_exc(), '#general')
         logger.error(traceback.format_exc())
         raise(traceback.format_exc())
